546408c7a4e747c1ee103604715de216f3e337b8
Hassanfach <73231569+Hassanfach@users.noreply.github.com>
2025-07-17 20:10:13 +0700

    Create install_elasticsearch_kibana.sh

---
+++ scripts/install_elasticsearch_kibana.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Install Elasticsearch and Kibana
+curl -fsSL https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
+echo "deb https://artifacts.elastic.co/packages/7.x/apt stable main" | sudo tee -a /etc/apt/sources.list.d/elastic-7.x.list
+sudo apt update
+sudo apt install -y elasticsearch kibana
